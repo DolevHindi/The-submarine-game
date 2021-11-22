2c47e815b70e4df63471c1e16753168e156c6c4a
Dolev Hindi <dolev.hindi1@gmail.com>
2021-11-22 15:49:41 +0200

    first commit

---
+++ Json.js
@@ -0,0 +1,20 @@
+﻿submarinesArr = [
+
+    {
+        size: 2,
+        amount: 2
+    },
+    {
+        size: 3,
+        amount: 3
+    },
+    {
+        size: 4,
+        amount: 2
+    },
+    {
+        size: 5,
+        amount: 1
+    }
+]
+
